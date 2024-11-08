7695957657fb1a4c514c3ee22e1d1b3b47bde855
dykciak <dawid.dykto@gmail.com>
2024-11-08 23:37:27 +0100

    finished and learned useEffect hook

--- src/App.jsx
+++ src/App.jsx
@@ -1,74 +1,110 @@
-import { useRef, useState } from 'react';
+import { useRef, useState, useEffect, useCallback } from "react";
 
-import Places from './components/Places.jsx';
-import { AVAILABLE_PLACES } from './data.js';
-import Modal from './components/Modal.jsx';
-import DeleteConfirmation from './components/DeleteConfirmation.jsx';
-import logoImg from './assets/logo.png';
+import Places from "./components/Places.jsx";
+import { AVAILABLE_PLACES } from "./data.js";
+import Modal from "./components/Modal.jsx";
+import DeleteConfirmation from "./components/DeleteConfirmation.jsx";
+import logoImg from "./assets/logo.png";
+import { sortPlacesByDistance } from "./loc.js";
+
+const storedIDs = JSON.parse(localStorage.getItem("selectedPlaces")) || [];
+const storedPlaces = storedIDs.map((id) =>
+	AVAILABLE_PLACES.find((place) => place.id === id)
+);
 
 function App() {
-  const modal = useRef();
-  const selectedPlace = useRef();
-  const [pickedPlaces, setPickedPlaces] = useState([]);
+	const selectedPlace = useRef();
+	const [modalIsOpen, setModalIsOpen] = useState(false);
+	const [availablePlaces, setAvailablePlaces] = useState([]);
+	const [pickedPlaces, setPickedPlaces] = useState(storedPlaces);
 
-  function handleStartRemovePlace(id) {
-    modal.current.open();
-    selectedPlace.current = id;
-  }
+	useEffect(() => {
+		navigator.geolocation.getCurrentPosition((position) => {
+			const sortedPlaces = sortPlacesByDistance(
+				AVAILABLE_PLACES,
+				position.coords.latitude,
+				position.coords.longitude
+			);
 
-  function handleStopRemovePlace() {
-    modal.current.close();
-  }
+			setAvailablePlaces(sortedPlaces);
+		});
+	}, []);
 
-  function handleSelectPlace(id) {
-    setPickedPlaces((prevPickedPlaces) => {
-      if (prevPickedPlaces.some((place) => place.id === id)) {
-        return prevPickedPlaces;
-      }
-      const place = AVAILABLE_PLACES.find((place) => place.id === id);
-      return [place, ...prevPickedPlaces];
-    });
-  }
+	function handleStartRemovePlace(id) {
+		setModalIsOpen(true);
+		selectedPlace.current = id;
+	}
 
-  function handleRemovePlace() {
-    setPickedPlaces((prevPickedPlaces) =>
-      prevPickedPlaces.filter((place) => place.id !== selectedPlace.current)
-    );
-    modal.current.close();
-  }
+	function handleStopRemovePlace() {
+		setModalIsOpen(false);
+	}
 
-  return (
-    <>
-      <Modal ref={modal}>
-        <DeleteConfirmation
-          onCancel={handleStopRemovePlace}
-          onConfirm={handleRemovePlace}
-        />
-      </Modal>
+	function handleSelectPlace(id) {
+		setPickedPlaces((prevPickedPlaces) => {
+			if (prevPickedPlaces.some((place) => place.id === id)) {
+				return prevPickedPlaces;
+			}
+			const place = AVAILABLE_PLACES.find((place) => place.id === id);
+			return [place, ...prevPickedPlaces];
+		});
 
-      <header>
-        <img src={logoImg} alt="Stylized globe" />
-        <h1>PlacePicker</h1>
-        <p>
-          Create your personal collection of places you would like to visit or
-          you have visited.
-        </p>
-      </header>
-      <main>
-        <Places
-          title="I'd like to visit ..."
-          fallbackText={'Select the places you would like to visit below.'}
-          places={pickedPlaces}
-          onSelectPlace={handleStartRemovePlace}
-        />
-        <Places
-          title="Available Places"
-          places={AVAILABLE_PLACES}
-          onSelectPlace={handleSelectPlace}
-        />
-      </main>
-    </>
-  );
+		const storedIDs = JSON.parse(localStorage.getItem("selectedPlaces")) || [];
+		if (storedIDs.indexOf(id) === -1) {
+			localStorage.setItem(
+				"selectedPlaces",
+				JSON.stringify([id, ...storedIDs])
+			);
+		}
+	}
+
+	const handleRemovePlace = useCallback(function handleRemovePlace() {
+		setPickedPlaces((prevPickedPlaces) =>
+			prevPickedPlaces.filter((place) => place.id !== selectedPlace.current)
+		);
+		setModalIsOpen(false);
+
+		const storedIDs = JSON.parse(localStorage.getItem("selectedPlaces")) || [];
+		localStorage.setItem(
+			"selectedPlaces",
+			JSON.stringify(storedIDs.filter((id) => id !== selectedPlace.current))
+		);
+	}, []);
+
+	return (
+		<>
+			<Modal open={modalIsOpen}>
+				{modalIsOpen && (
+					<DeleteConfirmation
+						onCancel={handleStopRemovePlace}
+						onConfirm={handleRemovePlace}
+					/>
+				)}
+			</Modal>
+
+			<header>
+				<img src={logoImg} alt="Stylized globe" />
+				<h1>PlacePicker</h1>
+				<p>
+					Create your personal collection of places you would like to visit or
+					you have visited.
+				</p>
+			</header>
+			<main>
+				<Places
+					title="I'd like to visit ..."
+					fallbackText={"Select the places you would like to visit below."}
+					places={pickedPlaces}
+					onSelectPlace={handleStartRemovePlace}
+				/>
+				<Places
+					title="Available Places"
+					places={availablePlaces}
+					fallbackText="Sorting places by distance..."
+					onSelectPlace={handleSelectPlace}
+				/>
+			</main>
+		</>
+	);
 }
 
 export default App;
---
+++ src/components/ProgressBar.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import { useState, useEffect } from "react";
+
+export default function ProgressBar({ timer }) {
+	const [remainingTime, setRemainingTime] = useState(timer);
+	useEffect(() => {
+		const interval = setInterval(() => {
+			console.log("huj");
+			setRemainingTime((prevTime) => prevTime - 10);
+		}, 10);
+
+		return () => clearInterval(interval);
+	}, []);
+	return <progress value={remainingTime} max={timer} />;
+}
